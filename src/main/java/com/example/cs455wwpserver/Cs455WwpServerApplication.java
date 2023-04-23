b4ec573d8d478113b7296aae03c6bb394a896633
Eric Curtland <554scout@gmail.com>
2023-04-23 16:40:23 -0700

    Basic setup for HTTP server with imported ball from the app

---
+++ src/main/java/com/example/cs455wwpserver/Cs455WwpServerApplication.java
@@ -0,0 +1,33 @@
+package com.example.cs455wwpserver;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Cs455WwpServerApplication {
+    private long lastTime = System.nanoTime();
+
+    @Autowired
+    private Ball gameBall;
+    public static void main(String[] args) {
+
+        SpringApplication.run(Cs455WwpServerApplication.class, args);
+    }
+
+    @GetMapping("/getPosition")
+    public String givePosition() {
+        long curTime = System.nanoTime();
+        System.out.printf("curTime: %d\nlastTime: %d%n", curTime, this.lastTime);
+        gameBall.updatePosition((curTime - this.lastTime) / 1E9);
+        this.lastTime = curTime;
+        return gameBall.toString();
+    }
+
+
+
+}
